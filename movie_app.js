5cf4c57884256303d3cc3db21ae046f42ecb86b7
alexm <alex-m@outlook.com>
2022-11-08 14:29:01 -0800

    Added a title and rating attribute to each new list item; added event handling for clicking the sort buttons; added helper functions for sorting

--- movie_app.js
+++ movie_app.js
@@ -17,7 +17,7 @@ $("#rating-form").on("submit", function(event) {
 
     // Add movie rating info and delete button to list in DOM
     const $delBtn = $("<button class='del-button'>Delete</button>");
-    const $newEntry = $(`<li>Title: ${title}; Rating: ${rating}</li>`).append($delBtn);
+    const $newEntry = $(`<li title='${title}' rating='${rating}'>Title: ${title}; Rating: ${rating}</li>`).append($delBtn);
     $("#movie-list").append($newEntry);
 
     $("#movie-title").val("");
@@ -29,7 +29,48 @@ $("#movie-list").on("click", ".del-button", function() {
     $(this).parent().remove();
 })
 
+/* Event handler for clicking either of the 'sort' buttons. */
+$(".sort").on("click", function() {
+    const listItems = $("#movie-list").children();
+    listItems.remove();
+
+    let listArr = listItems.get();
+
+    if ($(this).attr("id") === "title-sort") {
+        listArr.sort(sortByTitle);
+    }
+
+    if ($(this).attr("id") === "rating-sort") {
+        listArr.sort(sortByRating);
+    }
+    
+    $("#movie-list").append($(listArr));
+})
+
 /* Event handler for clicking the 'clear list' button for the movie list. */
 $("#clear-list").on("click", function() {
     $("#movie-list").children().remove();
 })
+
+
+// Helper functions -------------------------------------------------------------------------------
+
+/** Helper function: sort an array of tags by their 'title' attribute (ascending). */
+const sortByTitle = (tag1, tag2) => {
+    const title1 = tag1.getAttribute("title").toLowerCase();
+    const title2 = tag2.getAttribute("title").toLowerCase();
+
+    if (title1 > title2) return 1;
+    if (title1 < title2) return -1;
+    return 0;
+}
+
+/** Helper function: sort an array of tags by their 'rating' attribute (descending). */
+const sortByRating = (tag1, tag2) => {
+    const rating1 = tag1.getAttribute("rating");
+    const rating2 = tag2.getAttribute("rating");
+
+    if (rating1 > rating2) return -1;
+    if (rating1 < rating2) return 1;
+    return 0;
+}

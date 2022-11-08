b09bb1296dceb171a80bfd745444d73ea31c9465
alexm <alex-m@outlook.com>
2022-11-07 21:49:42 -0800

    Added a 'submit' event handler to the rating form

--- movie_app.js
+++ movie_app.js
@@ -1,3 +1,18 @@
 // Ioana Alex Mititean
 // 11/7/22
 // 13.1.14: jQuery Exercises, PART TWO
+
+// Attach a 'submit' event handler to rating form
+$("#rating-form").on("submit", function (event) {
+    event.preventDefault();
+
+    const title = $("#movie-title").val();
+    const rating = $("#movie-rating").val();
+
+    // Add movie rating info to list in DOM
+    $("#movie-list").append(`<li>Title: ${title}; Rating: ${rating}</li>`);
+
+    $("#movie-title").val("");
+    $("#movie-rating").val("");
+})
+

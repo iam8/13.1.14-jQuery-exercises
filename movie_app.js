ef935261620312ffda69751db48d463648c1080c
alexm <alex-m@outlook.com>
2022-11-07 22:30:11 -0800

    Created a delete button for each created movie list item; added event handler for these buttons

--- movie_app.js
+++ movie_app.js
@@ -3,20 +3,28 @@
 // 13.1.14: jQuery Exercises, PART TWO
 
 /* Event handler for submitting the movie rating form. */
-$("#rating-form").on("submit", function (event) {
+$("#rating-form").on("submit", function(event) {
     event.preventDefault();
 
     const title = $("#movie-title").val();
     const rating = $("#movie-rating").val();
 
+    const $delBtn = $("<button class='del-button'>Delete</button>");
+
     // Add movie rating info to list in DOM
-    $("#movie-list").append(`<li>Title: ${title}; Rating: ${rating}</li>`);
+    const $newEntry = $(`<li>Title: ${title}; Rating: ${rating}</li>`).append($delBtn);
+    $("#movie-list").append($newEntry);
 
     $("#movie-title").val("");
     $("#movie-rating").val("");
 })
 
+/* Event handler for clicking the 'delete' button next to any list item. */
+$("#movie-list").on("click", ".del-button", function() {
+    $(this).parent().remove();
+})
+
 /* Event handler for clicking the 'clear list' button for the movie list. */
-$("#clear-list").on("click", function () {
+$("#clear-list").on("click", function() {
     $("#movie-list").children().remove();
 })
